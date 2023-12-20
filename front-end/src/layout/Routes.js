9ab1dbe02c8e700d97bde75296eccfb58cad8c18
Colin Bauer <129115657+cbauer14@users.noreply.github.com>
2023-12-20 09:58:21 -0700

    updated routes

--- front-end/src/layout/Routes.js
+++ front-end/src/layout/Routes.js
@@ -1,9 +1,14 @@
 import React from "react";
-
 import { Redirect, Route, Switch } from "react-router-dom";
 import Dashboard from "../dashboard/Dashboard";
 import NotFound from "./NotFound";
 import { today } from "../utils/date-time";
+import CreateTable from "../tables/CreateTable";
+import CreateReservation from "../reservations/CreateReservation";
+import EditReservation from "../reservations/EditReservation";
+import TableSelect from "../tables/TableSelect";
+import SearchReservation from "../reservations/SearchReservation";
+import useQuery from "../utils/useQuery";
 
 /**
  * Defines all the routes for the application.
@@ -12,7 +17,11 @@ import { today } from "../utils/date-time";
  *
  * @returns {JSX.Element}
  */
+
 function Routes() {
+  const query = useQuery();
+  const date = query.get("date");
+
   return (
     <Switch>
       <Route exact={true} path="/">
@@ -22,7 +31,22 @@ function Routes() {
         <Redirect to={"/dashboard"} />
       </Route>
       <Route path="/dashboard">
-        <Dashboard date={today()} />
+        <Dashboard date={date ? date : today()} />
+      </Route>
+      <Route exact={true} path="/reservations/new">
+        <CreateReservation />
+      </Route>
+      <Route exact={true} path="/reservations/:reservation_id/seat">
+        <TableSelect />
+      </Route>
+      <Route exact={true} path="/reservations/:reservation_id/edit">
+        <EditReservation />
+      </Route>
+      <Route exact={true} path="/tables/new">
+        <CreateTable />
+      </Route>
+      <Route exact={true} path="/search">
+        <SearchReservation />
       </Route>
       <Route>
         <NotFound />
