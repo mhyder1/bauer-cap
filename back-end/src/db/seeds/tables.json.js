91f3e3b1761ae543e58eb2974d5ef82607f1f77c
Colin Bauer <129115657+cbauer14@users.noreply.github.com>
2023-12-20 12:04:46 -0700

    Reloaded migration tables

--- back-end/src/db/seeds/tables.json.js
+++ back-end/src/db/seeds/tables.json.js
@@ -1,13 +1,18 @@
-
-exports.seed = function(knex) {
-  // Deletes ALL existing entries
-  return knex('table_name').del()
-    .then(function () {
-      // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
-      ]);
-    });
-};
+[
+  {
+      "table_name": "Bar #1",
+      "capacity": 1
+  },
+  {
+      "table_name": "Bar #2",
+      "capacity": 1
+  },
+  {
+      "table_name": "#1",
+      "capacity": 6
+  },
+  {
+      "table_name": "#2",
+      "capacity": 6
+  }
+]
