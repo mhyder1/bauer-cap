e8e5e04871969dba253de590ad4c9bf5e5f2215d
Colin Bauer <129115657+cbauer14@users.noreply.github.com>
2023-12-20 13:11:30 -0700

    Update migrations and seeds

--- back-end/src/db/migrations/20231220123912_createReservations.js
+++ back-end/src/db/migrations/20231220123912_createReservations.js
@@ -1,8 +1,16 @@
-
-exports.up = function(knex) {
+exports.up = function (knex) {
+    return knex.schema.createTable("reservations", (table) => {
+      table.increments("reservation_id").primary();
+      table.string("first_name").notNullable();
+      table.string("last_name").notNullable();
+      table.string("mobile_number").notNullable();
+      table.date("reservation_date").notNullable();
+      table.time("reservation_time", { precision: 6}).notNullable();
+      table.integer("people").unsigned().notNullable();
+      table.timestamps(true, true);
+    });
+  };
   
-};
-
-exports.down = function(knex) {
-  
-};
+  exports.down = function (knex) {
+    return knex.schema.dropTable("reservations");
+  };
