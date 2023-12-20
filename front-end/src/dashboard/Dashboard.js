f185650a3c93b5c113a68eaa4b2ed1878b51c90a
Colin Bauer <129115657+cbauer14@users.noreply.github.com>
2023-12-20 09:57:14 -0700

    updated dashboard

--- front-end/src/dashboard/Dashboard.js
+++ front-end/src/dashboard/Dashboard.js
@@ -1,6 +1,10 @@
 import React, { useEffect, useState } from "react";
-import { listReservations } from "../utils/api";
+import { finishReservation, listReservations, listTable } from "../utils/api";
+import { next, previous, today } from "../utils/date-time";
+import { useHistory } from "react-router-dom";
 import ErrorAlert from "../layout/ErrorAlert";
+import ReservationList from "../reservations/ReservationList";
+import TableList from "../tables/TableList";
 
 /**
  * Defines the dashboard page.
@@ -10,7 +14,10 @@ import ErrorAlert from "../layout/ErrorAlert";
  */
 function Dashboard({ date }) {
   const [reservations, setReservations] = useState([]);
+  const [tables, setTables] = useState([]);
   const [reservationsError, setReservationsError] = useState(null);
+
+  const history = useHistory();
 
   useEffect(loadDashboard, [date]);
 
@@ -23,14 +30,87 @@ function Dashboard({ date }) {
     return () => abortController.abort();
   }
 
+  useEffect(loadTables, []);
+  function loadTables() {
+    const abortController = new AbortController();
+    listTable(abortController.signal).then(setTables);
+    return () => abortController.abort();
+  }
+
+  const handleFinishReservation = async (table_id) => {
+    const abortController = new AbortController();
+    const confirm = window.confirm(
+      "Is this table ready to seat new guests?\nThis cannot be undone."
+    );
+    if (confirm) {
+      try {
+        await finishReservation(table_id, abortController.signal);
+        loadDashboard();
+        loadTables();
+      } catch (e) {
+        console.log(e);
+      }
+    }
+    return () => abortController.abort();
+  };
+
+  function previousDay(date) {
+    const previousDate = previous(date);
+    history.push(`/dashboard?date=${previousDate}`);
+  }
+
+  function nextDay(date) {
+    const nextDate = next(date);
+    history.push(`/dashboard?date=${nextDate}`);
+  }
+
   return (
     <main>
-      <h1>Dashboard</h1>
+      <div className="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom  d-md-flex">
+        <div>
+          <h1>Dashboard</h1>
+          <h5>Today's Date: {date}</h5>
+        </div>
+
+        <div className="btn-toolbar mb-2 mb-md-0">
+          <div className="btn-group me-2">
+            <button
+              type="button"
+              className="btn btn-outline-secondary btn m-1 mt-2 float-right"
+              onClick={() => previousDay(date)}
+            >
+              Previous Day
+            </button>
+            <button
+              type="button"
+              className="btn btn-outline-secondary btn m-1 mt-2 float-right"
+              onClick={() => history.push(`/dashboard?date=${today()}`)}
+            >
+              Today
+            </button>
+            <button
+              type="button"
+              className="btn btn-outline-secondary btn m-1 mt-2 float-right"
+              onClick={() => nextDay(date)}
+            >
+              Next Day
+            </button>
+          </div>
+        </div>
+      </div>
+
       <div className="d-md-flex mb-3">
         <h4 className="mb-0">Reservations for date</h4>
       </div>
       <ErrorAlert error={reservationsError} />
-      {JSON.stringify(reservations)}
+      <ReservationList
+        reservations={reservations}
+        loadDashboard={loadDashboard}
+      />
+      <TableList
+        tables={tables}
+        handleFinishReservation={handleFinishReservation}
+      />
     </main>
   );
 }
